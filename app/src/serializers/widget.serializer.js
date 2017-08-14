68656ebcf89216f70a72de467f3d52ee0b59b680
rrequero <rrequero@gmail.com>
2017-08-14 14:40:56 +0200

    add include vocabulary

--- app/src/serializers/widget.serializer.js
+++ app/src/serializers/widget.serializer.js
@@ -4,25 +4,26 @@ class WidgetSerializer {
             id: el._id,
             type: 'widget',
             attributes: {
-                name:		 el.name,
-		dataset:	 el.dataset,
-		slug:		 el.slug,
-		userId:          el.userId,
-		description:	 el.description,
-		source:		 el.source,
-		sourceUrl:	 el.sourceUrl,
-		authors:	 el.authors,
-		application:	 el.application,
-		verified:	 el.verified,
-		default:	 el.default,
-		published:       el.published,
-		queryUrl:        el.queryUrl,
-		widgetConfig:    el.widgetConfig,
-		template:        el.template,
-                layerId:         el.layerId,
-		createdAt:       el.createdAt,
-		updatedAt:       el.updatedAt
-	    }
+                name: el.name,
+                dataset: el.dataset,
+                slug: el.slug,
+                userId: el.userId,
+                description: el.description,
+                source: el.source,
+                sourceUrl: el.sourceUrl,
+                authors: el.authors,
+                application: el.application,
+                verified: el.verified,
+                default: el.default,
+                published: el.published,
+                queryUrl: el.queryUrl,
+                widgetConfig: el.widgetConfig,
+                template: el.template,
+                layerId: el.layerId,
+                createdAt: el.createdAt,
+                updatedAt: el.updatedAt,
+                vocabulary: el.vocabulary
+            }
         };
     }
 
@@ -33,7 +34,7 @@ class WidgetSerializer {
                 result.data = data.docs.map(el => WidgetSerializer.serializeElement(el));
             } else {
                 if (Array.isArray(data)) {
-		    result.data = data.map(el => WidgetSerializer.serializeElement(el));
+                    result.data = data.map(el => WidgetSerializer.serializeElement(el));
                     // result.data = WidgetSerializer.serializeElement(data[0]);
                 } else {
                     result.data = WidgetSerializer.serializeElement(data);
