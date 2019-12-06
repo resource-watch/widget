b7bddc1576ef68ff9914f1344a303fd5586e86b4
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-12-06 12:06:02 +0000

    Silence errors for screenshot failures when creating/updating widgets

--- app/test/e2e/widget-update.spec.js
+++ app/test/e2e/widget-update.spec.js
@@ -372,6 +372,120 @@ describe('Update widgets tests', () => {
         new Date(databaseWidget.updatedAt).should.beforeDate(new Date());
     });
 
+    it('Updating a widget when taking a snapshot fails should still succeed', async () => {
+        const widgetOne = await new Widget(createWidget()).save();
+
+        nock(`${process.env.CT_URL}/v1`)
+            .get(`/dataset/${widgetOne.dataset}`)
+            .twice()
+            .reply(200, {
+                data: {
+                    id: widgetOne.dataset,
+                    type: 'dataset',
+                    attributes: {
+                        name: 'Seasonal variability',
+                        slug: 'Seasonal-variability',
+                        type: null,
+                        subtitle: null,
+                        application: [
+                            'rw'
+                        ],
+                        dataPath: null,
+                        attributesPath: null,
+                        connectorType: 'rest',
+                        provider: 'cartodb',
+                        userId: '1a10d7c6e0a37126611fd7a7',
+                        connectorUrl: 'https://wri-01.carto.com/tables/rw_projections_20150309/public',
+                        tableName: 'rw_projections_20150309',
+                        status: 'pending',
+                        published: true,
+                        overwrite: false,
+                        verified: false,
+                        blockchain: {},
+                        mainDateField: null,
+                        env: 'production',
+                        geoInfo: false,
+                        protected: false,
+                        legend: {
+                            date: [],
+                            region: [],
+                            country: [],
+                            nested: []
+                        },
+                        clonedHost: {},
+                        errorMessage: null,
+                        taskId: null,
+                        updatedAt: '2018-11-19T11:45:44.405Z',
+                        dataLastUpdated: null,
+                        widgetRelevantProps: [],
+                        layerRelevantProps: []
+                    }
+                }
+            });
+
+        const widget = {
+            name: 'Widget default',
+            queryUrl: 'query/5be16fea-5b1a-4daf-a9e9-9dc1f6ea6d4e?sql=select * from crops',
+            application: [
+                'rw'
+            ],
+            description: 'widget description',
+            source: 'widget source',
+            sourceUrl: 'http://bar.foo',
+            authors: '',
+            status: 1,
+            default: true,
+            published: true,
+            widgetConfig: { ...widgetConfig, someNewProp: 'someNewValue' }
+        };
+
+        let databaseWidget = await Widget.findById(widgetOne.id).exec();
+
+        databaseWidget.name.should.not.equal(widget.name);
+        databaseWidget.description.should.not.equal(widget.description);
+        databaseWidget.sourceUrl.should.not.equal(widget.sourceUrl);
+        databaseWidget.queryUrl.should.not.equal(widget.queryUrl);
+        databaseWidget.widgetConfig.should.not.deep.equal(widget.widgetConfig);
+        new Date(databaseWidget.updatedAt).should.beforeDate(new Date());
+
+        // Mock failed screenshot request
+        nock(`${process.env.CT_URL}`)
+            .post(uri => uri.match(/\/v1\/webshot\/widget\/(\w|-)*\/thumbnail/))
+            .reply(500);
+
+        const response = await requester
+            .patch(`/api/v1/widget/${widgetOne.id}`)
+            .send({
+                dataset: widgetOne.dataset,
+                widget,
+                loggedUser: USERS.ADMIN
+            });
+
+        response.status.should.equal(200);
+        response.body.should.have.property('data').and.be.an('object');
+
+        const updatedWidget = response.body.data;
+
+        updatedWidget.attributes.name.should.equal(widget.name);
+        updatedWidget.attributes.description.should.equal(widget.description);
+        updatedWidget.attributes.dataset.should.equal(widgetOne.dataset);
+        updatedWidget.attributes.sourceUrl.should.equal(widget.sourceUrl);
+        updatedWidget.attributes.queryUrl.should.equal(widget.queryUrl);
+        updatedWidget.attributes.widgetConfig.should.deep.equal(widget.widgetConfig);
+        updatedWidget.attributes.updatedAt.should.not.equal(databaseWidget.updatedAt);
+        new Date(updatedWidget.attributes.updatedAt).should.equalDate(new Date());
+
+        databaseWidget = await Widget.findById(widgetOne.id).exec();
+
+        databaseWidget.name.should.equal(widget.name);
+        databaseWidget.description.should.equal(widget.description);
+        databaseWidget.sourceUrl.should.equal(widget.sourceUrl);
+        databaseWidget.queryUrl.should.equal(widget.queryUrl);
+        databaseWidget.widgetConfig.should.deep.equal(widget.widgetConfig);
+        databaseWidget.widgetConfig.should.deep.equal(widget.widgetConfig);
+        new Date(databaseWidget.updatedAt).should.equalDate(new Date());
+    });
+
     afterEach(() => {
         if (!nock.isDone()) {
             throw new Error(`Not all nock interceptors were used: ${nock.pendingMocks()}`);
