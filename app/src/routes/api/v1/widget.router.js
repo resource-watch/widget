296e969128cff2435b93bbe93158ad32f1b508c5
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-01-08 15:03:51 +0000

    Remove page size limit

--- app/src/routes/api/v1/widget.router.js
+++ app/src/routes/api/v1/widget.router.js
@@ -186,9 +186,9 @@ class WidgetRouter {
             )));
         }
 
-        if (query['page[size]'] && query['page[size]'] > 100) {
-            ctx.throw(400, 'Invalid page size');
-        }
+        // if (query['page[size]'] && query['page[size]'] > 100) {
+        //     ctx.throw(400, 'Invalid page size');
+        // }
 
         if (Object.keys(query).find(el => el.indexOf('collection') >= 0)) {
             if (!userId) {
