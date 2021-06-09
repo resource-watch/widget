fc77589a8b26486e4399ee2b8c7c484b73057a36
Tiago Garcia <tiagojsag@gmail.com>
2021-06-09 14:31:53 +0100

    Add support for hosts from `x-rw-domain` header when generating pagination links

--- app/test/e2e/widget-get.spec.js
+++ app/test/e2e/widget-get.spec.js
@@ -61,6 +61,37 @@ describe('Get widgets tests', () => {
             response.body.links.should.have.property('first').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
             response.body.links.should.have.property('last').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
         });
+
+        it('Get widgets with x-rw-domain header should be successful and use that header on the links on the response', async () => {
+            const response = await requester
+                .get(`/api/v1/widget`)
+                .set('x-rw-domain', `potato.com`);
+
+            response.status.should.equal(200);
+            response.body.should.have.property('data').and.be.an('array');
+            response.body.should.have.property('links').and.be.an('object');
+            response.body.links.should.have.property('self').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
+            response.body.links.should.have.property('prev').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
+            response.body.links.should.have.property('next').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
+            response.body.links.should.have.property('first').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
+            response.body.links.should.have.property('last').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
+        });
+
+        it('Get widgets with x-rw-domain and referer headers should be successful and use the x-rw-domain header on the links on the response', async () => {
+            const response = await requester
+                .get(`/api/v1/widget`)
+                .set('x-rw-domain', `potato.com`)
+                .set('referer', `https://tomato.com/get-me-all-the-data`);
+
+            response.status.should.equal(200);
+            response.body.should.have.property('data').and.be.an('array');
+            response.body.should.have.property('links').and.be.an('object');
+            response.body.links.should.have.property('self').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
+            response.body.links.should.have.property('prev').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
+            response.body.links.should.have.property('next').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
+            response.body.links.should.have.property('first').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
+            response.body.links.should.have.property('last').and.equal('http://potato.com/v1/widget?page[number]=1&page[size]=10');
+        });
     });
 
     it('Get all widgets should be successful and return an empty list (empty db)', async () => {
